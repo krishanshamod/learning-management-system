52b198e54feb0e1ca86c784af3d9ebf28359476f
pasandevin <jayawardene.pasandevin@gmail.com>
2022-04-13 02:58:28 +0530

    getgrade method

--- src/main/java/com/uok/backend/course/registration/CourseRegistration.java
+++ src/main/java/com/uok/backend/course/registration/CourseRegistration.java
@@ -57,6 +57,16 @@ public class CourseRegistration {
     //TODO
     // should implement the proper method
     public char getGrade() {
+        if (marks >= 75 && marks <= 100)
+            return 'A';
+        else if (marks >= 65 && marks <= 74)
+            return 'B';
+        else if (marks >= 55 && marks <= 64)
+            return 'C';
+        else if (marks >= 35 && marks <= 54)
+            return 'S';
+        else if (marks >= 0 && marks <= 34)
+            return 'F';
         return grade;
     }
 
