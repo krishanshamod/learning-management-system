d591108d8604c64fc949a362da9914ef3d41b9cc
Krishan Shamod <krishanshamod@gmail.com>
2022-04-13 09:09:27 +0530

    Updated the JWT Request Filter

--- src/main/java/com/uok/backend/security/JwtRequestFilter.java
+++ src/main/java/com/uok/backend/security/JwtRequestFilter.java
@@ -23,24 +23,32 @@ public class JwtRequestFilter extends OncePerRequestFilter {
     @Autowired
     private TokenValidator tokenValidator;
 
+    public static String validatedToken = null;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws ServletException, IOException {
 
+        // get token from request
         final String authorizationHeader = request.getHeader("Authorization");
 
         String email = null;
         String jwt = null;
 
+        // extract the token
         if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
             jwt = authorizationHeader.substring(7);
             email = tokenValidator.getEmailFromToken(jwt);
         }
 
+        // validate token
         if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = userDetailsService.loadUserByUsername(email);
 
             if (tokenValidator.validate(jwt)) {
+
+                validatedToken = jwt;
+
                 UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
                         userDetails, null, userDetails.getAuthorities());
 
