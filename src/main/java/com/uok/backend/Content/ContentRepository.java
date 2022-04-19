f9ae59cbead11c56501959472eafeea494620fcd
Krishan Shamod <krishanshamod@gmail.com>
2022-04-20 00:34:17 +0530

    Updated ContentRepository

--- src/main/java/com/uok/backend/Content/ContentRepository.java
+++ src/main/java/com/uok/backend/Content/ContentRepository.java
@@ -1,6 +1,5 @@
 package com.uok.backend.Content;
 
-import com.uok.backend.course.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +8,6 @@ import java.util.List;
 @Repository
 public interface ContentRepository extends JpaRepository<Content, ContentId> {
     List<Content> findByCourseId(String courseId);
+
+    Content findByCourseIdAndTitle(String courseId, String title);
 }
