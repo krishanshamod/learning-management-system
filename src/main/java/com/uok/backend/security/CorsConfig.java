c581ec2f66036d2a60be89ba70d960d8261bafd8
Krishan Shamod <krishanshamod@gmail.com>
2022-04-23 10:42:39 +0530

    Updated CORS Config

--- src/main/java/com/uok/backend/security/CorsConfig.java
+++ src/main/java/com/uok/backend/security/CorsConfig.java
@@ -6,14 +6,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*");
+                registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*");
             }
         };
     }
