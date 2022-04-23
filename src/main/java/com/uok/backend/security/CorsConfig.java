0ddff10f26e4594c39b5dd63e764e0d832eb329b
Krishan Shamod <krishanshamod@gmail.com>
2022-04-23 11:53:20 +0530

    Updated CORS Config

--- src/main/java/com/uok/backend/security/CorsConfig.java
+++ src/main/java/com/uok/backend/security/CorsConfig.java
@@ -15,8 +15,7 @@ public class CorsConfig implements WebMvcConfigurer {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("*")
-                        .allowedHeaders("*")
-                        .allowedMethods("*");
+                        .allowedHeaders("*");
             }
         };
     }
