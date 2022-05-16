1d875a0859570d0873f960a0dd2ce303e0589687
pasandevin <jayawardene.pasandevin@gmail.com>
2022-05-16 21:32:23 +0530

    added test for successful course registration

---
+++ src/test/java/com/uok/backend/course/LMSCourseServiceTest.java
@@ -0,0 +1,91 @@
+package com.uok.backend.course;
+
+import com.uok.backend.course.registration.CourseRegistration;
+import com.uok.backend.course.registration.CourseRegistrationRepository;
+import com.uok.backend.security.TokenValidator;
+import com.uok.backend.user.LMSUserService;
+import com.uok.backend.user.User;
+import com.uok.backend.user.UserRepository;
+import com.uok.backend.user.UserService;
+import com.uok.backend.utils.Logger;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ContextConfiguration;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ContextConfiguration(classes = {LMSCourseService.class})
+@ExtendWith(MockitoExtension.class)
+class LMSCourseServiceTest {
+    @Autowired
+    private LMSCourseService underTest;
+
+    @Mock
+    private CourseRepository courseRepository ;
+    @Mock
+    private CourseRegistrationRepository courseRegistrationRepository;
+    @Mock
+    private UserService userService;
+    @Mock
+    private Logger logger;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new LMSCourseService(courseRepository, courseRegistrationRepository, userService, logger);
+    }
+
+
+    @Test
+    @Disabled
+    void shouldAddNewCourse() {
+        //given
+        Course course = new Course("cf", "Computer Fundamentals");
+        User user = new User("pasandevin@gmail.com", "Pasan", "Jayawardene", "student");
+
+        //when
+        when(userService.getTokenUser()).thenReturn(user);
+        ResponseEntity response = underTest.addNewCourse(course);
+
+        //then
+        ArgumentCaptor<Course> courseArgumentCaptor = ArgumentCaptor.forClass(Course.class);
+        verify(courseRepository).save(courseArgumentCaptor.capture());
+        Course capturedCourse = courseArgumentCaptor.getValue();
+        assertThat(capturedCourse).isEqualTo(course);
+
+        ArgumentCaptor<String> emailArgumentCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> courseIdArgumentCaptor = ArgumentCaptor.forClass(String.class);
+
+        verify(courseRegistrationRepository).addUserToCourse(emailArgumentCaptor.capture(), courseIdArgumentCaptor.capture());
+        String capturedEmail = emailArgumentCaptor.getValue();
+        String capturedCourseId = courseIdArgumentCaptor.getValue();
+        assertThat(capturedEmail).isEqualTo(user.getEmail());
+        assertThat(capturedCourseId).isEqualTo(capturedCourse.getId());
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+
+    @Test
+    void addUserToCourse() {
+    }
+
+    @Test
+    void getEnrolledCourses() {
+    }
+
+    @Test
+    void getAvailableCourses() {
+    }
+}
