1fb1240806e685fa19d7120f1386a19e196fc755
Krishan Shamod <krishanshamod@gmail.com>
2022-05-19 23:34:39 +0530

    Added addAnnouncement integration tests

---
+++ src/test/java/com/uok/backend/announcement/AnnouncementControllerIntegrationTest.java
@@ -0,0 +1,79 @@
+package com.uok.backend.announcement;
+
+import com.uok.backend.BackendApplication;
+import com.uok.backend.content.Content;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = BackendApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class AnnouncementControllerIntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    HttpHeaders headers = new HttpHeaders();
+
+    @Test
+    void addAnnouncement_newAnnouncementDetailsGiven_shouldReturnHttp200() {
+
+        headers.add(
+                HttpHeaders.AUTHORIZATION,
+                "Bearer eyJhbGciOiJIUzUxMiJ9.eyJlbWFpbCI6InRlc3RsZWN0dXJlckB0ZXN0LmNvbSI" +
+                        "sImZpcnN0TmFtZSI6ImZpcnN0TmFtZSIsImxhc3ROYW1lIjoibGFzdE5hbWUiLCJyb2xlIjoi" +
+                        "bGVjdHVyZXIifQ.Kfl5jfcuJ_P8KARsRpYWyRNaV_gRC7dVlUjgXgOlqRds81SF2Di6u5bsTHr" +
+                        "9nrLsv9lN4xlnwcN6b3zvmU0s4Q"
+        );
+
+        Announcement announcement = new Announcement("testcourseid", "testAnnouncementTitle1", "testAnnouncementContent1");
+
+        HttpEntity<Announcement> entity = new HttpEntity<Announcement>(announcement, headers);
+
+        ResponseEntity response = restTemplate.exchange(
+                createURLWithPort("/announcement/addannouncement"),
+                HttpMethod.POST, entity, String.class);
+
+        assertEquals(200, response.getStatusCodeValue());
+    }
+
+    @Test
+    void addAnnouncement_existingAnnouncementDetailsGiven_shouldReturnHttp400() {
+
+        headers.add(
+                HttpHeaders.AUTHORIZATION,
+                "Bearer eyJhbGciOiJIUzUxMiJ9.eyJlbWFpbCI6InRlc3RsZWN0dXJlckB0ZXN0LmNvbSI" +
+                        "sImZpcnN0TmFtZSI6ImZpcnN0TmFtZSIsImxhc3ROYW1lIjoibGFzdE5hbWUiLCJyb2xlIjoi" +
+                        "bGVjdHVyZXIifQ.Kfl5jfcuJ_P8KARsRpYWyRNaV_gRC7dVlUjgXgOlqRds81SF2Di6u5bsTHr" +
+                        "9nrLsv9lN4xlnwcN6b3zvmU0s4Q"
+        );
+
+        Announcement announcement = new Announcement("testcourseid", "testAnnouncementTitle", "testAnnouncementContent");
+
+        HttpEntity<Announcement> entity = new HttpEntity<Announcement>(announcement, headers);
+
+        ResponseEntity response = restTemplate.exchange(
+                createURLWithPort("/announcement/addannouncement"),
+                HttpMethod.POST, entity, String.class);
+
+        assertEquals(400, response.getStatusCodeValue());
+    }
+
+    private String createURLWithPort(String uri) {
+        return "http://localhost:" + port + uri;
+    }
+
+}
