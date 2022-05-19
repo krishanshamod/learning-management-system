c05c5df96311d57039c464bc609c04a4a3bc8805
Krishan Shamod <krishanshamod@gmail.com>
2022-05-19 22:24:42 +0530

    Added enrolledCourses and availableCoueses integration tests

--- src/test/java/com/uok/backend/course/CourseControllerIntegrationTest.java
+++ src/test/java/com/uok/backend/course/CourseControllerIntegrationTest.java
@@ -93,6 +93,46 @@ class CourseControllerIntegrationTest {
         assertEquals(200, response.getStatusCodeValue());
     }
 
+    @Test
+    void checkEnrolledCourses_studentUserGiven_shouldReturnHttp200() {
+
+        headers.add(
+                HttpHeaders.AUTHORIZATION,
+                "Bearer eyJhbGciOiJIUzUxMiJ9.eyJlbWFpbCI6InRlc3RzdHVkZW50QHRlc3QuY29t" +
+                        "IiwiZmlyc3ROYW1lIjoiZmlyc3ROYW1lIiwibGFzdE5hbWUiOiJsYXN0TmFtZSIsInJvbGU" +
+                        "iOiJzdHVkZW50In0.cvVfmD0me4ob6ekr_x-HnWaI786bQNkyTlLgYxtiFQwEPTpuWB9dS5" +
+                        "JfRguZIT0fjW1SETVQRuJ4WkE9w-wlzg"
+        );
+
+        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+
+        ResponseEntity response = restTemplate.exchange(
+                createURLWithPort("/course/enrolledcourses"),
+                HttpMethod.GET, entity, String.class);
+
+        assertEquals(200, response.getStatusCodeValue());
+    }
+
+    @Test
+    void checkAvailableCourses_studentUserGiven_shouldReturnHttp200() {
+
+        headers.add(
+                HttpHeaders.AUTHORIZATION,
+                "Bearer eyJhbGciOiJIUzUxMiJ9.eyJlbWFpbCI6InRlc3RzdHVkZW50QHRlc3QuY29t" +
+                        "IiwiZmlyc3ROYW1lIjoiZmlyc3ROYW1lIiwibGFzdE5hbWUiOiJsYXN0TmFtZSIsInJvbGU" +
+                        "iOiJzdHVkZW50In0.cvVfmD0me4ob6ekr_x-HnWaI786bQNkyTlLgYxtiFQwEPTpuWB9dS5" +
+                        "JfRguZIT0fjW1SETVQRuJ4WkE9w-wlzg"
+        );
+
+        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+
+        ResponseEntity response = restTemplate.exchange(
+                createURLWithPort("/course/availablecourses"),
+                HttpMethod.GET, entity, String.class);
+
+        assertEquals(200, response.getStatusCodeValue());
+    }
+
     private String createURLWithPort(String uri) {
         return "http://localhost:" + port + uri;
     }
